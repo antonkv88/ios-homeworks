3cc77e36ff5c7122abf63385ca3c9b3bf3395178
Антон Козлов <antonkozlov@mac-mini-anton.beeline>
2023-04-15 15:02:39 +0300

    Initial Commit

---
+++ navigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  navigation
+//
+//  Created by Антон Козлов on 15.04.2023.
+//  Copyright © 2023 Антон Козлов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
