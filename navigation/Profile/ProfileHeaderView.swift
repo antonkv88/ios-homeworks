72d5840b481708e23013e4242b2546d53f75829c
Антон Козлов <antonkozlov@mac-mini-anton.local>
2023-04-23 00:58:48 +0300

    Реализация события на кнопку. Вывод в консоль текста статуса

--- navigation/Profile/ProfileHeaderView.swift
+++ navigation/Profile/ProfileHeaderView.swift
@@ -2,6 +2,11 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     
+    var imageProfile : UIImageView?
+    var labelProfileName : UILabel?
+    var labelStatus : UILabel?
+    var button : UIButton?
+    
     func createButton() -> UIButton {
         let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 50))
         button.backgroundColor = .blue
@@ -36,47 +41,48 @@ class ProfileHeaderView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        let imageProfile = createImage(#imageLiteral(resourceName: "hipsterDog"))
-        self.addSubview(imageProfile)
-        imageProfile.translatesAutoresizingMaskIntoConstraints = false
+        imageProfile = createImage(#imageLiteral(resourceName: "hipsterDog"))
+        self.addSubview(imageProfile!)
+        imageProfile!.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            imageProfile.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 16),
-            imageProfile.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor, constant: 16),
-            imageProfile.widthAnchor.constraint(equalToConstant: 120),
-            imageProfile.heightAnchor.constraint(equalToConstant: 120)
+            imageProfile!.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 16),
+            imageProfile!.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor, constant: 16),
+            imageProfile!.widthAnchor.constraint(equalToConstant: 120),
+            imageProfile!.heightAnchor.constraint(equalToConstant: 120)
         ])
         
-        let button = createButton()
-        self.addSubview(button)
-        button.translatesAutoresizingMaskIntoConstraints = false
+        button = createButton()
+        self.addSubview(button!)
+        button!.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            button.topAnchor.constraint(equalTo: imageProfile.safeAreaLayoutGuide.bottomAnchor, constant: 16),
-            button.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor, constant: 16),
-            button.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor, constant: -16),
-            button.heightAnchor.constraint(equalToConstant: 50),
-            button.widthAnchor.constraint(equalToConstant: 120)
+            button!.topAnchor.constraint(equalTo: imageProfile!.safeAreaLayoutGuide.bottomAnchor, constant: 16),
+            button!.leftAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leftAnchor, constant: 16),
+            button!.rightAnchor.constraint(equalTo: self.safeAreaLayoutGuide.rightAnchor, constant: -16),
+            button!.heightAnchor.constraint(equalToConstant: 50),
+            button!.widthAnchor.constraint(equalToConstant: 120)
         ])
         
-        let labelProfileName = createLabel("Brutal Dog", color : .black, fontSize : 18, textStyle : .bold)
-        self.addSubview(labelProfileName)
-        labelProfileName.translatesAutoresizingMaskIntoConstraints = false
+        labelProfileName = createLabel("Brutal Dog", color : .black, fontSize : 18, textStyle : .bold)
+        self.addSubview(labelProfileName!)
+        labelProfileName!.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            labelProfileName.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 27),
-            labelProfileName.leftAnchor.constraint(equalTo: imageProfile.safeAreaLayoutGuide.rightAnchor, constant: 30),
+            labelProfileName!.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 27),
+            labelProfileName!.leftAnchor.constraint(equalTo: imageProfile!.safeAreaLayoutGuide.rightAnchor, constant: 30),
         ])
         
-        let labelStatus = createLabel("Waiting to something...", color : .gray, fontSize : 14, textStyle : .regular)
-        self.addSubview(labelStatus)
-        labelStatus.translatesAutoresizingMaskIntoConstraints = false
+        labelStatus = createLabel("Waiting to something...", color : .gray, fontSize : 14, textStyle : .regular)
+        self.addSubview(labelStatus!)
+        labelStatus!.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            labelStatus.bottomAnchor.constraint(equalTo: button.safeAreaLayoutGuide.topAnchor, constant: -34),
-            labelStatus.leftAnchor.constraint(equalTo: imageProfile.safeAreaLayoutGuide.rightAnchor, constant: 30),
+            labelStatus!.bottomAnchor.constraint(equalTo: button!.safeAreaLayoutGuide.topAnchor, constant: -34),
+            labelStatus!.leftAnchor.constraint(equalTo: imageProfile!.safeAreaLayoutGuide.rightAnchor, constant: 30),
             ])
-
+        
+        button!.addTarget(self, action: #selector(buttonPressed), for : UIControlEvents.touchUpInside)
     }
     
     @objc func buttonPressed(){
-      self.subviews.
+        print(labelStatus!.text!)
     }
     
     
